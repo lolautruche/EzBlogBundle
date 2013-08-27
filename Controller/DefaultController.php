072bbf2e3bf3762b501fa2cf56224047d99f200e
Jérôme Vieilledent <lolautruche@gmail.com>
2013-08-27 18:32:31 +0200

    Blog posts list: Implemented custom controller

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,12 +2,41 @@
 
 namespace Acme\EzBlogBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use eZ\Bundle\EzPublishCoreBundle\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function listBlogPostsAction( $locationId )
     {
-        return $this->render('AcmeEzBlogBundle:Default:index.html.twig', array('name' => $name));
+        $response = new Response();
+        // Set a 1h max age in cache
+        $response->setSharedMaxAge( 3600 );
+        // Make the response location cache aware for the reverse proxy
+        $response->headers->set( 'X-Location-Id', $locationId );
+
+        $request = $this->getRequest();
+        $offset = $request->query->get( 'offset', 0 );
+        // TODO: Make this limit configurable.
+        $limit = 10;
+
+        // Use Public API to load current location/content and its children (posts)
+        // TODO: We might use the search API instead
+        $repository = $this->getRepository();
+        $locationService = $repository->getLocationService();
+        $contentService = $repository->getContentService();
+        $location = $locationService->loadLocation( $locationId );
+        $content = $contentService->loadContentByContentInfo( $location->getContentInfo() );
+        $children = $locationService->loadLocationChildren( $location, $offset, $limit );
+
+        return $this->render(
+            'AcmeEzBlogBundle:full:blog.html.twig',
+            array(
+                'location' => $location,
+                'content' => $content,
+                'posts' => $children
+            ),
+            $response
+        );
     }
 }
